c5c6e507f72b8dbf2eb2a0069fa8547294c4a066
Amr Alaa <n1amr1@gmail.com>
2016-02-04 23:04:13 +0200

    misprediction penalty

--- HazardDetectionUnit.v
+++ HazardDetectionUnit.v
@@ -1,8 +1,8 @@
-module HazardDetectionUnit(Branch, ID_EX_MemRead, ID_EX_RegisterRt, ID_EX_RegWrite, ID_EX_Write_Reg, EX_MEM_MemRead, EX_MEM_RegisterRt, IF_ID_RegisterRs, IF_ID_RegisterRt, control_flush, pc_freeze, IF_ID_freeze);
+module HazardDetectionUnit(Comparator_result, Branch, ID_EX_MemRead, ID_EX_RegisterRt, ID_EX_RegWrite, ID_EX_Write_Reg, EX_MEM_MemRead, EX_MEM_RegisterRt, IF_ID_RegisterRs, IF_ID_RegisterRt, control_flush, pc_freeze, IF_ID_freeze, take_branch, IF_ID_flush);
 	
-	input Branch, ID_EX_MemRead, EX_MEM_MemRead, ID_EX_RegWrite;
+	input Comparator_result, Branch, ID_EX_MemRead, EX_MEM_MemRead, ID_EX_RegWrite;
 	input [4:0] ID_EX_RegisterRt, ID_EX_Write_Reg, EX_MEM_RegisterRt, IF_ID_RegisterRs, IF_ID_RegisterRt;
-	output reg control_flush, pc_freeze, IF_ID_freeze;
+	output reg control_flush, pc_freeze, IF_ID_freeze, take_branch, IF_ID_flush;
 
 	always @(Branch or ID_EX_MemRead or EX_MEM_MemRead or ID_EX_RegWrite or ID_EX_RegisterRt or ID_EX_Write_Reg or EX_MEM_RegisterRt or IF_ID_RegisterRs or IF_ID_RegisterRt)	begin
 		if (
@@ -26,6 +26,17 @@ module HazardDetectionUnit(Branch, ID_EX_MemRead, ID_EX_RegisterRt, ID_EX_RegWri
 			control_flush = 1'b0;
 			pc_freeze = 1'b0;
 			IF_ID_freeze = 1'b0;
+		end
+	end
+
+	always @(Branch or Copmarator_result) begin
+		if(Branch && Copmarator_result == 1'b1) begin
+			take_branch == 1'b1;
+			IF_ID_flush == 1'b1;
+		end
+		else begin
+			take_branch == 1'b0;
+			IF_ID_flush == 1'b0;
 		end
 	end
 endmodule
