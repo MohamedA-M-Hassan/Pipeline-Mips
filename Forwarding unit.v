dc1ab6ee36c6db4147c1847a7d1b17ce58123a6c
MohamedA-M-Hassan <anehmr0@gmail.com>
2016-02-04 17:27:29 +0200

    Forwarding unit

--- Forwarding unit.v
+++ Forwarding unit.v
@@ -1,34 +1,35 @@
 module ForwardingUnit (MEM_WB_RegWrite ,EX_MEM_RegWrite, EX_MEM_RegisterRd ,
 						MEM_WB_RegisterRd , ID_EX_RegisterRs , ID_EX_RegisterRt , ForwardA , ForwardB);
-	input   [31:0] MEM_WB_RegWrite , EX_MEM_RegWrite , EX_MEM_RegisterRd ,
+	input    MEM_WB_RegWrite , EX_MEM_RegWrite , EX_MEM_RegisterRd ,
 					MEM_WB_RegisterRd , ID_EX_RegisterRs , ID_EX_RegisterRt ;
 	
-	output   [1:0] ForwardA , ForwardB ;
+	output  reg [1:0] ForwardA , ForwardB ;
 	
- 
-			// Ex Hazard
+	initial 
+		begin
+			// Ex Hazard   A
 			if (EX_MEM_RegWrite && (EX_MEM_RegisterRd != 0)
 				&& (EX_MEM_RegisterRd == ID_EX_RegisterRs))
 			
-			assign	ForwardA = 10 ;
+				ForwardA = 2'b10 ;
 			
+			// Mem Hazard A
+			else if (MEM_WB_RegWrite && (MEM_WB_RegisterRd != 0)
+				&& (MEM_WB_RegisterRd == ID_EX_RegisterRs))
+	
+				ForwardA = 2'b01 ;
+			// Ex Hazard B  	
 			if (EX_MEM_RegWrite && (EX_MEM_RegisterRd != 0)
 				&& (EX_MEM_RegisterRd == ID_EX_RegisterRt))
 			
-			assign	ForwardB = 10  ;
+				ForwardB = 2'b10  ;
 				
-				// Mem Hazard 
-			if (MEM_WB_RegWrite && (MEM_WB_RegisterRd != 0)
-				!&(EX_MEM_RegWrite && (EX_MEM_RegisterRd != 0) && (EX_MEM_RegisterRd == ID_EX_RegisterRs))
-				&& (MEM_WB_RegisterRd == ID_EX_RegisterRs))
-	
-			assign	ForwardA = 01 ;
-				
-			if ( MEM_WB_RegWrite && (MEM_WB_RegisterRd != 0)
-				!& (EX_MEM_RegWrite && (EX_MEM_RegisterRd != 0) && (EX_MEM_RegisterRd == ID_EX_RegisterRt))
+		
+			// // Mem Hazard B	
+			else if ( MEM_WB_RegWrite && (MEM_WB_RegisterRd != 0)
 				&& (MEM_WB_RegisterRd == ID_EX_RegisterRt))
 
-			assign	ForwardB = 01  ;
-				
+				ForwardB = 2'b01;
+		end		
 		
 endmodule
