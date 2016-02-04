22a35d2f055eaf1241165762cbb469e172857bfc
Amr Alaa <n1amr1@gmail.com>
2016-02-04 20:36:42 +0200

    fix harazd detection unit

--- HazardDetectionUnit.v
+++ HazardDetectionUnit.v
@@ -1,28 +1,21 @@
-module  HazardDetectionUnit( ID_EX_MemRead , ID_EX_RegisterRt , IF_ID_RegisterRs , IF_ID_RegisterRt ,
-							stall , freeze , IF_ID_Write);
+module  HazardDetectionUnit(ID_EX_MemRead, ID_EX_RegisterRt, IF_ID_RegisterRs, IF_ID_RegisterRt, stall, freeze, IF_ID_Write);
 	
-	input ID_EX_MemRead , ID_EX_RegisterRt , IF_ID_RegisterRs , IF_ID_RegisterRt ;
-	output reg stall , freeze , IF_ID_Write ;
+	input ID_EX_MemRead;
+	input [4:0] ID_EX_RegisterRt, IF_ID_RegisterRs, IF_ID_RegisterRt;
+	output reg stall, freeze, IF_ID_Write;
 
-	initial
-	begin
-		if 	(ID_EX_MemRead && 
-			( (ID_EX_RegisterRt == IF_ID_RegisterRs) || (ID_EX_RegisterRt == IF_ID_RegisterRt) ) )
-			begin
-				
-				stall = 1'b1 ;
-				freeze = 1'b1;
-				IF_ID_Write = 1'b1 ;
-				
-			end	
-		else  
-			begin
-				
-				stall = 1'b0 ;
-				freeze = 1'b0;
-				IF_ID_Write = 1'b0 ;
-				
-			end
-			
-	end		   
-endmodule	
+	always @(ID_EX_MemRead or ID_EX_RegisterRt or IF_ID_RegisterRs or IF_ID_RegisterRt)	begin
+		if (ID_EX_MemRead && (
+				 (ID_EX_RegisterRt == IF_ID_RegisterRs) ||
+				 (ID_EX_RegisterRt == IF_ID_RegisterRt)
+				)) begin
+			stall = 1'b1;
+			freeze = 1'b1;
+			IF_ID_Write = 1'b1;
+		end else begin
+			stall = 1'b0;
+			freeze = 1'b0;
+			IF_ID_Write = 1'b0;
+		end
+	end
+endmodule
