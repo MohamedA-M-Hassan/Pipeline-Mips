93d2d904da98d15e16f6ee9b52dd2ff172c2836e
Amr Alaa <n1amr1@gmail.com>
2016-02-04 20:50:57 +0200

    rename harard detection unit signals

--- HazardDetectionUnit.v
+++ HazardDetectionUnit.v
@@ -1,21 +1,21 @@
-module  HazardDetectionUnit(ID_EX_MemRead, ID_EX_RegisterRt, IF_ID_RegisterRs, IF_ID_RegisterRt, stall, freeze, IF_ID_Write);
+module HazardDetectionUnit(ID_EX_MemRead, ID_EX_RegisterRt, IF_ID_RegisterRs, IF_ID_RegisterRt, control_flush, pc_freeze, IF_ID_freeze);
 	
 	input ID_EX_MemRead;
 	input [4:0] ID_EX_RegisterRt, IF_ID_RegisterRs, IF_ID_RegisterRt;
-	output reg stall, freeze, IF_ID_Write;
+	output reg control_flush, pc_freeze, IF_ID_freeze;
 
 	always @(ID_EX_MemRead or ID_EX_RegisterRt or IF_ID_RegisterRs or IF_ID_RegisterRt)	begin
 		if (ID_EX_MemRead && (
 				 (ID_EX_RegisterRt == IF_ID_RegisterRs) ||
 				 (ID_EX_RegisterRt == IF_ID_RegisterRt)
 				)) begin
-			stall = 1'b1;
-			freeze = 1'b1;
-			IF_ID_Write = 1'b1;
+			control_flush = 1'b1;
+			pc_freeze = 1'b1;
+			IF_ID_freeze = 1'b1;
 		end else begin
-			stall = 1'b0;
-			freeze = 1'b0;
-			IF_ID_Write = 1'b0;
+			control_flush = 1'b0;
+			pc_freeze = 1'b0;
+			IF_ID_freeze = 1'b0;
 		end
 	end
 endmodule
