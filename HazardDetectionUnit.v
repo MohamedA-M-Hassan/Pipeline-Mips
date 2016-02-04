213e7a388e557ad97069e688b42f35bc1e1bf053
Amr Alaa <n1amr1@gmail.com>
2016-02-04 22:39:17 +0200

    branch stalls

--- HazardDetectionUnit.v
+++ HazardDetectionUnit.v
@@ -1,14 +1,24 @@
-module HazardDetectionUnit(ID_EX_MemRead, ID_EX_RegisterRt, IF_ID_RegisterRs, IF_ID_RegisterRt, control_flush, pc_freeze, IF_ID_freeze);
+module HazardDetectionUnit(ID_EX_MemRead, ID_EX_RegisterRt, ID_EX_RegWrite, ID_EX_Write_Reg, EX_MEM_MemRead, EX_MEM_RegisterRt, IF_ID_RegisterRs, IF_ID_RegisterRt, control_flush, pc_freeze, IF_ID_freeze);
 	
-	input ID_EX_MemRead;
-	input [4:0] ID_EX_RegisterRt, IF_ID_RegisterRs, IF_ID_RegisterRt;
+	input ID_EX_MemRead, EX_MEM_MemRead, ID_EX_RegWrite;
+	input [4:0] ID_EX_RegisterRt, ID_EX_Write_Reg, EX_MEM_RegisterRt, IF_ID_RegisterRs, IF_ID_RegisterRt;
 	output reg control_flush, pc_freeze, IF_ID_freeze;
 
-	always @(ID_EX_MemRead or ID_EX_RegisterRt or IF_ID_RegisterRs or IF_ID_RegisterRt)	begin
-		if (ID_EX_MemRead && (
-				 (ID_EX_RegisterRt == IF_ID_RegisterRs) ||
-				 (ID_EX_RegisterRt == IF_ID_RegisterRt)
-				)) begin
+	always @(	ID_EX_MemRead or EX_MEM_MemRead or ID_EX_RegWrite or ID_EX_RegisterRt or ID_EX_Write_Reg or EX_MEM_RegisterRt or IF_ID_RegisterRs or IF_ID_RegisterRt)	begin
+		if (
+			  (ID_EX_MemRead && (
+						 (ID_EX_RegisterRt == IF_ID_RegisterRs) ||
+						 (ID_EX_RegisterRt == IF_ID_RegisterRt)
+				)) ||
+				(EX_MEM_MemRead && (
+						 (EX_MEM_RegisterRt == IF_ID_RegisterRs) ||
+						 (EX_MEM_RegisterRt == IF_ID_RegisterRt)
+						)) ||
+				(ID_EX_RegWrite && !ID_EX_MemRead && (
+						 (ID_EX_Write_Reg == IF_ID_RegisterRs) ||
+						 (ID_EX_Write_Reg == IF_ID_RegisterRt)
+						))
+				) begin
 			control_flush = 1'b1;
 			pc_freeze = 1'b1;
 			IF_ID_freeze = 1'b1;
